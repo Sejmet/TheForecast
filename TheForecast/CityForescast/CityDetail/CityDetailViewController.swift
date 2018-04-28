60749d631415e9139026a2bbfa3b38029669640c
Maria Belen Marañon <>
2018-04-28 20:18:27 -0300

    [CityDetailViewController] adding ui componentes and outlet for ui components

--- TheForecast/CityForescast/CityDetail/CityDetailViewController.swift
+++ TheForecast/CityForescast/CityDetail/CityDetailViewController.swift
@@ -7,29 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
 class CityDetailViewController: UIViewController {
 
+    @IBOutlet weak var dataContainerView: UIView!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var minimumTemperatureLabel: UILabel!
+    @IBOutlet weak var maximumTemperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var cityMapView: MKMapView!
+    
+    let detailViewModel: CityDetailViewModel = CityDetailViewModel()
+    var cityId: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        dataFromViewModel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func dataFromViewModel() {
+        detailViewModel.cityWeatherById(cityId: cityId) { (error: Error?) in
+            if error == nil {
+                self.setupCityDetail()
+            }
+        }
     }
-    */
-
+    
+    func setupCityDetail() {
+        
+    }
 }
