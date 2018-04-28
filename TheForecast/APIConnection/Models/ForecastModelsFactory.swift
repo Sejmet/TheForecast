a15d497bc786db8650cd4c6b882251f49c18a214
Maria Belen Marañon <>
2018-04-27 21:30:34 -0300

    [ForecastModelsFactory] create class & adding func to decode a City Weather Object

---
+++ TheForecast/APIConnection/Models/ForecastModelsFactory.swift
@@ -0,0 +1,23 @@
+//
+//  ForecastModelsFactory.swift
+//  TheForecast
+//
+//  Created by Maria Belen Marañon on 27/4/18.
+//  Copyright © 2018 IT Crowd Challenge. All rights reserved.
+//
+
+import UIKit
+
+class ForecastModelsFactory: NSObject {
+
+    public func makeCityWeatherObject(json: Data) -> CityWeatherObject? {
+        do {
+            let result = try JSONDecoder().decode(CityWeatherObject.self, from: json)
+            return result
+        } catch {
+            print("Error parsing a CityWeatherObject")
+            return nil
+        }
+    }
+    
+}
