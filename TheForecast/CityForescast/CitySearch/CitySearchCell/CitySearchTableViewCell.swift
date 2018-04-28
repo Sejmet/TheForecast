b90463b2b84d0ee6effb0d16e8a71708e06e57eb
Maria Belen Marañon <>
2018-04-28 20:52:50 -0300

    Create CitySearchTableViewCell for search

---
+++ TheForecast/CityForescast/CitySearch/CitySearchCell/CitySearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CitySearchTableViewCell.swift
+//  TheForecast
+//
+//  Created by Maria Belen Marañon on 28/4/18.
+//  Copyright © 2018 IT Crowd Challenge. All rights reserved.
+//
+
+import UIKit
+
+class CitySearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
