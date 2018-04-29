bd0f61b38ac532a0778901bfdd415575ce1f1c45
Maria Belen Marañon <>
2018-04-29 19:13:02 -0300

    Create FiveDaysWeatherObject & CoreDataModelManager

---
+++ TheForecast/APIConnection/Models/FiveDaysWeatherObject.swift
@@ -0,0 +1,13 @@
+//
+//  FiveDaysWeatherObject.swift
+//  TheForecast
+//
+//  Created by Maria Belen Marañon on 29/4/18.
+//  Copyright © 2018 IT Crowd Challenge. All rights reserved.
+//
+
+import UIKit
+
+class FiveDaysWeatherObject: NSObject {
+
+}
---
+++ TheForecast/CityForescast/CoreDataManager/CoreDataModelManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataModelManager.swift
+//  TheForecast
+//
+//  Created by Maria Belen Marañon on 29/4/18.
+//  Copyright © 2018 IT Crowd Challenge. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataModelManager: NSObject {
+
+}
