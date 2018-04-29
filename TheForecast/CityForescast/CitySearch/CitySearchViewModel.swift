48a2cc78eca1884cc1f50ba90f33df18595b61e3
Maria Belen Marañon <>
2018-04-29 15:46:43 -0300

    [CitySearchVM] adding func to save, delete and fetch object from Core Data. Use LittleCityList json file instead of hardcoded cities

--- TheForecast/CityForescast/CitySearch/CitySearchViewModel.swift
+++ TheForecast/CityForescast/CitySearch/CitySearchViewModel.swift
@@ -7,20 +7,112 @@
 //
 
 import UIKit
+import CoreData
 
 class CitySearchViewModel: NSObject {
 
     var cityArray: [CityObject] = []
     var filteredCityArray: [CityObject] = []
+    var coreDataCityArray: [CityObject] = []
+    
+    private var forecastFactory: ForecastModelsFactory = ForecastModelsFactory()
     
     func populateCityArray() {
-        cityArray.append(CityObject(id: 707860, name: "Hurzuf", country: "RU", coord: nil))
-        cityArray.append(CityObject(id: 122335, name: "Gohuris", country: "RU", coord: nil))
-        cityArray.append(CityObject(id: 12225, name: "Gorkhā", country: "RU", coord: nil))
-        cityArray.append(CityObject(id: 123555, name: "Buenos Aires", country: "AR", coord: nil))
+        cityArray = []
+        if let path = Bundle.main.path(forResource: "LittleCityList", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                
+                if let cityResponse = forecastFactory.makeCityResponseObject(json: data) {
+                    if let cities = cityResponse.city {
+                        cityArray = cities
+                    }
+                }
+            } catch {
+                print("Error trying to find Little City List json file")
+            }
+        }
     }
     
     func filterCityByName(cityName: String) {
         filteredCityArray = cityArray.filter { ($0.name?.localizedCaseInsensitiveContains(cityName))! }
+    }
+    
+    func populateCoreDataCityArray() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "City")
+        request.returnsObjectsAsFaults = false
+        
+        self.coreDataCityArray = []
+        
+        do {
+            let result = try context.fetch(request)
+            if let resultArray = result as? [CityObject] {
+                for city in resultArray {
+                    self.coreDataCityArray.append(city)
+                }
+            }
+        } catch {
+            print("Failed")
+        }
+    }
+    
+    func persistCity(cityObject: CityObject) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        checkFivePersistObject()
+        
+        let cityEntity = NSEntityDescription.entity(forEntityName: "City", in: context)
+        let newCity = NSManagedObject(entity: cityEntity!, insertInto: context)
+        newCity.setValue(cityObject.id, forKey: "id")
+        newCity.setValue(cityObject.name, forKey: "name")
+        newCity.setValue(cityObject.country, forKey: "country")
+        
+        saveCoreDataContext()
+    }
+    
+    func checkFivePersistObject() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "City")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let result = try context.fetch(request)
+            if let resultArray = result as? [NSManagedObject] {
+                if resultArray.count == 5 {
+                    context.delete(resultArray[0])
+                }
+            }
+        } catch {
+            print("Failed")
+        }
+        
+        saveCoreDataContext()
+    }
+    
+    func saveCoreDataContext() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        do {
+            try context.save()
+        } catch {
+            print("Failed saving")
+        }
     }
 }
