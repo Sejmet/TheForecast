af549e15874f72e0d798b2dcbe0754b9537b8700
Maria Belen Marañon <>
2018-04-29 00:48:59 -0300

    [CitySearchVC] complete flow to cityDetailVC. Use searchViewModel data to populate cityTableView. Call filter func from view model when search bar text did change

--- TheForecast/CityForescast/CitySearch/CitySearchViewController.swift
+++ TheForecast/CityForescast/CitySearch/CitySearchViewController.swift
@@ -13,10 +13,13 @@ class CitySearchViewController: UIViewController {
     @IBOutlet weak var citySearchBar: UISearchBar!
     @IBOutlet weak var cityTableView: UITableView!
     
+    let searchViewModel: CitySearchViewModel = CitySearchViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupTableView()
+        setupSearchBar()
     }
 
     func setupTableView() {
@@ -28,21 +31,44 @@ class CitySearchViewController: UIViewController {
         
         cityTableView.register(UINib(nibName: "CitySearchTableViewCell", bundle: nil), forCellReuseIdentifier: "CitySearchTableViewCell")
     }
+    
+    func setupSearchBar() {
+        searchViewModel.populateCityArray()
+        citySearchBar.delegate = self
+    }
 
+}
+
+extension CitySearchViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchViewModel.filterCityByName(cityName: searchText)
+        cityTableView.reloadData()
+    }
 }
 
 extension CitySearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return searchViewModel.filteredCityArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CitySearchTableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CitySearchTableViewCell", for: indexPath) as! CitySearchTableViewCell
         
+        cell.setupCellWith(cityName: searchViewModel.filteredCityArray[indexPath.row].name)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        let detailVC = CityDetailViewController(nibName: "CityDetailViewController", bundle: nil)
+        detailVC.cityObject = searchViewModel.filteredCityArray[indexPath.row]
+        navigationController?.pushViewController(detailVC, animated: true)
     }
 }
