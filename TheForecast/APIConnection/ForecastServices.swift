770eeff49c41bc03e195a5004017fd5b94a951e9
Maria Belen Marañon <>
2018-04-29 00:46:03 -0300

    [ForecastServices] change cityid from string to int

--- TheForecast/APIConnection/ForecastServices.swift
+++ TheForecast/APIConnection/ForecastServices.swift
@@ -15,7 +15,7 @@ class ForecastServices: NSObject {
     private var apiHost: String = "api.openweathermap.org/data/2.5/weather?"
     private var forecastFactory: ForecastModelsFactory = ForecastModelsFactory()
     
-    public func cityWeatherById(cityId: String, completion: @escaping (_ cityObject: CityWeatherObject?, _ error: Error?) -> Void) {
+    public func cityWeatherById(cityId: Int, completion: @escaping (_ cityObject: CityWeatherObject?, _ error: Error?) -> Void) {
         let urlString: String = "\(apiHost)/id=\(cityId)&appid=\(openWeatherApiKey)&units=metric"
         if let url: URL = URL(string: urlString) {
             Alamofire.request(url).responseJSON { (response: DataResponse<Any>) in
