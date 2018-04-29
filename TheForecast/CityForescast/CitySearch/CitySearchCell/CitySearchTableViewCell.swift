333a67d2b82898edf09963f06be7e5733741d033
Maria Belen Marañon <>
2018-04-28 21:01:26 -0300

    [CitySearchTVC] adding code for cell

--- TheForecast/CityForescast/CitySearch/CitySearchCell/CitySearchTableViewCell.swift
+++ TheForecast/CityForescast/CitySearch/CitySearchCell/CitySearchTableViewCell.swift
@@ -10,15 +10,10 @@ import UIKit
 
 class CitySearchTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    func setupCellWith(cityName: String) {
+        cityNameLabel.text = cityName
     }
     
 }
