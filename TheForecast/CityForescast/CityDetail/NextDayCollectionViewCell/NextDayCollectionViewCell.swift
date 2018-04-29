b7499479d4598714d5a9d01edb40850644bc4120
Maria Belen Marañon <>
2018-04-29 19:38:19 -0300

    Create collection view cell for next five days collection view

---
+++ TheForecast/CityForescast/CityDetail/NextDayCollectionViewCell/NextDayCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NextDayCollectionViewCell.swift
+//  TheForecast
+//
+//  Created by Maria Belen Marañon on 29/4/18.
+//  Copyright © 2018 IT Crowd Challenge. All rights reserved.
+//
+
+import UIKit
+
+class NextDayCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
