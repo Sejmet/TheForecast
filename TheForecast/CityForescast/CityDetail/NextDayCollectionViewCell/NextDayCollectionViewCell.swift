1849ff288fab17d5d164087c68a78249f046b8d3
Maria Belen Marañon <>
2018-04-29 19:45:46 -0300

    [NextDayCollectionViewCell] adding UI components and a func to set information

--- TheForecast/CityForescast/CityDetail/NextDayCollectionViewCell/NextDayCollectionViewCell.swift
+++ TheForecast/CityForescast/CityDetail/NextDayCollectionViewCell/NextDayCollectionViewCell.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class NextDayCollectionViewCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var minimumTemperatureLabel: UILabel!
+    @IBOutlet weak var maximumTemperatureLabel: UILabel!
+    
+    func setupCellWith(weatherInformation: WeatherObject, temperatureInformation: MainObject) {
+        if let minTemperature = temperatureInformation.temp_min {
+            minimumTemperatureLabel.text = "Min.:\(minTemperature)°C"
+        }
+        
+        if let maxTemperature = temperatureInformation.temp_max {
+            maximumTemperatureLabel.text = "Max.:\(maxTemperature)°C"
+        }
     }
-
 }
