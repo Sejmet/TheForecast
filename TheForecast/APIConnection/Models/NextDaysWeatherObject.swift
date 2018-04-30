fffc806a46f285700d77f03c1065671c5b7721f4
Maria Belen Marañon <>
2018-04-29 21:00:25 -0300

    Adding assets & change name of class FiveDaysWeather to NextDaysWeather

---
+++ TheForecast/APIConnection/Models/NextDaysWeatherObject.swift
@@ -0,0 +1,13 @@
+//
+//  NextDaysWeatherObject.swift
+//  TheForecast
+//
+//  Created by Maria Belen Marañon on 29/4/18.
+//  Copyright © 2018 IT Crowd Challenge. All rights reserved.
+//
+
+import UIKit
+
+class NextDaysWeatherObject: NSObject {
+
+}
